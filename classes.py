e58891f580994948678dc1150e636b1a57e03787
castrombithisamm <castro.mbithii@gmail.com>
2020-07-08 16:54:51 +0300

    added new code

---
+++ classes.py
@@ -0,0 +1,14 @@
+class PartyAnimal:
+    x = 0
+
+    def party(self):
+        self.x = self.x + 1
+        print("So far", self.x)
+
+an = PartyAnimal()
+
+an.party()
+an.party()
+an.party()
+print("Type", type(an))
+print("Dir", dir(an))
